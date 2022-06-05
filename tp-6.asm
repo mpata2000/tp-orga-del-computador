6fbd9866208950c58a62eb5041e3cf0abd2ba8e4
Martin Pata Fraile de Manerola <64995978+mpata2000@users.noreply.github.com>
2022-06-05 00:02:28 -0300

    Ingreso primitivo de datos

--- tp-6.asm
+++ tp-6.asm
@@ -22,17 +22,81 @@
 
 global main
 extern printf
+extern puts
+extern gets
+extern sscanf
 
 section     .data
-    mensaje1 db "Numero: %li",10,0
-    vector dq 321,32,23,12,34
-    contador dq 0
+    mensaje_ingresar_destino    db "Ingrese el destino:",0
+    formato_destino             db "%c"
+    mensaje_ingresar_peso       db "Ingrese un numero etre el 1 y 13 inclusive",0
+    formato_peso                db "%i"
+    mensaje1                    db "Numero: %lli",0
 
 section     .bss
+    input    resb   500    
+    destino  resb   1
+    peso     resd   1
 
+
+; TODO Ingreso Destino
+; TODO Ingreso Peso
 
 section     .text
 main:
+    ;ingresar destino
+    mov rcx,mensaje_ingresar_destino
+    sub rsp,32
+    call puts
+    add rsp,32
+
+    mov rcx,input
+    sub rsp,32
+    call gets
+    add rsp,32
+
+    ;paso primer caracter a destino
+    mov ah,[input]
+    mov byte[destino],ah
+
+    ;mov rcx,input
+    ;mov rdx,formato_destino
+    ;mov r8,destino
+    ;sub rsp,32
+    ;call sscanf
+    ;add rsp,32
+    
+    mov rcx,destino
+    sub rsp,32
+    call puts
+    add rsp,32
+
+    mov rcx,mensaje_ingresar_peso
+    sub rsp,32
+    call puts
+    add rsp,32
+
+    mov rcx,input
+    sub rsp,32
+    call gets
+    add rsp,32
+
+    mov rcx,input
+    mov rdx,formato_peso
+    mov r8,peso
+    sub rsp,32
+    call sscanf
+    add rsp,32
+    
+    mov rcx,mensaje1
+    mov rdx,[peso]
+    sub rsp,32
+    call printf
+    add rsp,32
+
+    
+
+
 
 
 ret
