3c50c3f868eee76d7e9757c707e19f142c4f116f
Martin Pata Fraile de Manerola <64995978+mpata2000@users.noreply.github.com>
2022-06-05 22:40:38 -0300

    Chequeo de ingreso valido

--- tp-6.asm
+++ tp-6.asm
@@ -28,28 +28,25 @@ extern sscanf
 
 section     .data
     mensaje_ingresar_destino    db "Ingrese el destino:",0
+    mensaje_destino_invalido    db "El destino no es valido. Los posibles destinos son Mar del Plata(M), Bariloche(B) y Posadas(P). Intente nuevamente:",0
     formato_destino             db "%c"
-    mensaje_ingresar_peso       db "Ingrese un numero etre el 1 y 13 inclusive",0
+    mensaje_ingresar_peso       db "Ingrese un numero etre el 1 y 11 inclusive",0
+    mensaje_peso_invalido       db "El peso ingresado es invalido. El peso debe estar entre 0 y 11 inclusive(0<p<=11). Intente nuevamente:",0
     formato_peso                db "%i"
-    mensaje1                    db "Numero: %lli",0
+    mensaje1                    db "Numero: %lli",10,0
 
 section     .bss
     input    resb   500    
     destino  resb   1
     peso     resd   1
 
-
-; TODO Ingreso Destino
-; TODO Ingreso Peso
-
 section     .text
 main:
-    ;ingresar destino
     mov rcx,mensaje_ingresar_destino
     sub rsp,32
     call puts
     add rsp,32
-
+ingresarDestino:
     mov rcx,input
     sub rsp,32
     call gets
@@ -66,16 +63,17 @@ main:
     ;call sscanf
     ;add rsp,32
     
-    mov rcx,destino
-    sub rsp,32
-    call puts
-    add rsp,32
 
+    call validarDestino
+    cmp rax,0
+    je ingresarDestino ;Si el destino no es valido se lo vuelve a pedir
+    
     mov rcx,mensaje_ingresar_peso
     sub rsp,32
     call puts
     add rsp,32
 
+ingresoPeso:
     mov rcx,input
     sub rsp,32
     call gets
@@ -87,16 +85,65 @@ main:
     sub rsp,32
     call sscanf
     add rsp,32
-    
+
+    call validarPeso
+    cmp rax,0
+    je ingresoPeso
+
     mov rcx,mensaje1
     mov rdx,[peso]
     sub rsp,32
     call printf
     add rsp,32
 
-    
+finPrograma:
+    ret
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;                                            ;
+;             VALIDAR DESTINO                ;
+;                                            ;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+validarDestino:
+    ;Destinos: Mar del Plata(M), Bariloche(B) y Posadas(P), 
+    mov rax,0
+    cmp byte[destino],"M"
+    je destinoValido
+    cmp byte[destino],"B"
+    je destinoValido
+    cmp byte[destino],"P"
+    je destinoValido
+
+    ;Destino no es valido
+    mov rcx,mensaje_destino_invalido
+    sub rsp,32
+    call puts
+    add rsp,32
+    ret
+destinoValido:
+    mov rax,1
+    ret
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;                                            ;
+;               VALIDAR PESO                 ;
+;                                            ;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+validarPeso:
+    ;verificando que 0<Pi<=11, devuelve rax=1 valido o rax=0 si es invalido
+    mov rax,1
+    cmp dword[peso],0
+    jle pesoInvalido ;Si es menor igual a 0 es invalido
+    cmp dword[peso],11
+    jg pesoInvalido
+    ret
+pesoInvalido:
+    ;Peso no es valido
+    mov rcx,mensaje_peso_invalido
+    sub rsp,32
+    call puts
+    add rsp,32
+    mov rax,0
+    ret
 
 
 
-
-ret
